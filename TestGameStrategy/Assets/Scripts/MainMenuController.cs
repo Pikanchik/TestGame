032bd2e9414e89970b89a859e75c750afb26e44e
Pikanchik <114467599+Pikanchik@users.noreply.github.com>
2022-09-27 12:04:25 +0300

    Add raw MainMenu

---
+++ TestGameStrategy/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Main Menu Buttons\n")]
+
+    [SerializeField]
+    private Button newGame;
+    
+    [SerializeField]
+    private Button loadGame;
+
+    [SerializeField]
+    private Button quit;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void NewGame()
+    {
+        
+    }
+}
